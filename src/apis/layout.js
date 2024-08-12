b64ec9c68ab2d1f81cb38ada043b86051a5934d5
yagao <949899039@qq.com>
2024-08-12 21:52:16 +0800

    6.渲染一级导航

--- src/apis/layout.js
+++ src/apis/layout.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import httpInstance from "@/utils/http"
 
-export function getCategory() {
+export function getCategoryAPI() {
   return httpInstance({
     url: '/home/category/head'
   })
