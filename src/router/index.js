037a0ea2b20391d668f1012b7a307af399764aa8
yagao <949899039@qq.com>
2024-08-14 12:50:38 +0800

    16.二级分类路由配置 面包屑导航 基础商品列表渲染

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Login from '@/views/Login/index.vue'
 import Layout from '@/views/Layout/index.vue'
 import Home from '@/views/Home/index.vue'
 import Category from '@/views/Category/index.vue'
+import SubCategory from '@/views/SubCategory/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,9 +21,11 @@ const router = createRouter({
         },
         {
           path: 'category/:id',
-          // path: 'category',
-          name: 'category',
           component: Category
+        },
+        {
+          path: 'category/sub/:id',
+          component: SubCategory
         }
       ]
     },
