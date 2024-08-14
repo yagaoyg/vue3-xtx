cc85fa9d0d5a1bf82a6deac515fd785e0de77cb3
yagao <949899039@qq.com>
2024-08-14 20:21:30 +0800

    19.图片预览组件实现 放大镜实现

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -4,6 +4,7 @@ import { getDetailAPI } from '@/apis/detail'
 import { onMounted, ref } from 'vue'
 import { useRoute, onBeforeRouteUpdate } from 'vue-router'
 import DetailHot from './components/DetailHot.vue'
+import ImageView from '@/components/ImageView/index.vue'
 
 const goodsData = ref({})
 const route = useRoute()
@@ -53,7 +54,7 @@ onMounted(() => {
           <div class="goods-info">
             <div class="media">
               <!-- 图片预览区 -->
-
+              <ImageView />
               <!-- 统计数量 -->
               <ul class="goods-sales">
                 <li>
@@ -137,9 +138,9 @@ onMounted(() => {
             <!-- 24热榜+专题推荐 -->
             <div class="goods-aside">
               <!-- 24小时 -->
-              <DetailHot :hot-type="1" />
+              <DetailHot :hot-type="'1'" />
               <!-- 周 -->
-              <DetailHot :hot-type="2" />
+              <DetailHot :hot-type="'2'" />
             </div>
           </div>
         </div>
