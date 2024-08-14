12fbfd737f5175942037234229618bb87b64aa05
yagao <949899039@qq.com>
2024-08-14 20:35:14 +0800

    21.图片预览优化

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -54,7 +54,7 @@ onMounted(() => {
           <div class="goods-info">
             <div class="media">
               <!-- 图片预览区 -->
-              <ImageView />
+              <ImageView :image-list="goodsData.mainPictures" />
               <!-- 统计数量 -->
               <ul class="goods-sales">
                 <li>
